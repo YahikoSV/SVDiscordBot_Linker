f6bfcb6498b425594018c9ef49527990a6fc9c4b
Yahiko_Daichi <42230158+YahikoSV@users.noreply.github.com>
2026-04-04 18:11:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import discord #discord python library
 from discord import app_commands  #discord commands
 from discord.ext import tasks, commands  #discord commands
 import os #get env file
-from playwright.async_api import async_playwright #headless browsing
-from playwright.sync_api import sync_playwright
+from playwright.async_api import async_playwright, expect #headless browsing
+import random #random choice
+
 
 
 from flask import Flask #keep alive
@@ -36,33 +37,65 @@ async def on_ready():
     print(f'We have logged in as {bot.user}')
 
 @bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    if message.content.startswith('大きな'):
-        await message.channel.send('すきだよ！')
-
-    if message.content.startswith('Is Rigze live?'):
-        response = await main()
-        await message.channel.send(response)
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound): #Happens when prefix is nothing
+        return  # Ignore the error quietly
+    raise error  # Propagate other errors to the console
 
 
-async def main():
+# @bot.event
+# async def on_message(message):
+#     if message.content.startswith('大きな'):
+#         await message.channel.send('すきだよ！')
+#  #       await bot.process_commands(message) #https://discordpy.readthedocs.io/en/latest/faq.html#why-does-on-message-make-my-commands-stop-working
+
+#     if message.content.startswith('Is Rigze live?'):
+#         response = await main(yt_name='rigze0925')
+#         await message.channel.send(response)
+#     await bot.process_commands(message)
+
+# @bot.listen('on_message')
+# async def asdf(message):
+#     if message.content.startswith('大きな'):
+#         await message.channel.send('すきだよ！')    
+
+@bot.command(name='大きな')
+async def shymm(ctx):
+    await ctx.send('すきだよ！')
+
+@bot.command(name='live?')
+async def check_yt_live(ctx, yt_name):
+    result = await main(yt_name)
+    await ctx.send(result)
+
+@bot.command(name='fbkcat', help='Responds with a random quote from Fubuki')
+async def Fubuki_Cat(ctx):
+    Fubuki_quotes = [
+        'No no cat! I\'m Fox', 
+        'Kitsune jyaa!', 
+        'Nyaajyanee yo!'
+    ]
+    response = random.choice(Fubuki_quotes)
+    await ctx.send(response)
+
+@bot.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
+async def main(yt_name):
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless = True)
+        browser = await p.chromium.launch(
+                headless = True
+                ,args=["--blink-settings=imagesEnabled=false"])
         page = await browser.new_page()
-        await page.goto("https://www.youtube.com/@rigze0925")
-        await page.wait_for_timeout(3000)
-        if await page.get_by_role("button", name="Tap to watch live").count() == 1:
+        await page.goto(f"https://www.youtube.com/@{yt_name}")
+
+        try: 
+            await expect(page.get_by_role("button", name="Tap to watch live")).to_be_visible(timeout=5000)  
             await page.get_by_role("button", name="Tap to watch live").click()
-            await page.wait_for_timeout(3000)
             response = page.url
-        else: 
-            response = 'Rigze is not live'
-        await browser.close()
+        except AssertionError as e:
+            response = f'{yt_name} is not live'
         return response
-
-
+    
 bot.run(TOKEN)
 
